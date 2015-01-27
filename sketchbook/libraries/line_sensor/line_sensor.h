8a2f4d671022ab3ba5cb26877223a29af40be0bc
Grumpy Uncle <gruncle@umkc>
2015-01-26 21:31:40 -0600

    found out that pointers in the arduino code are bad.
    need to redo the code to remove the pointers and have the
        setup routines get passed the pins for assignments.

--- sketchbook/libraries/line_sensor/line_sensor.h
+++ sketchbook/libraries/line_sensor/line_sensor.h
@@ -1,52 +1,80 @@
-#include <QTRSensors.h>
 
-//LED Thresholds
-    /*int[] cMinOn; //white with LED on
-    int[] cMaxOn; //black with LED on
-    int[] cMinOff; //white with LED off
-    int[] cMaxOff;  //black with LED off*/
+//#include<QTRSensors.h>
 
-    class LineSensors {
-    /* this will be altered in the future.
-       nothing in this class is set in stone
-       and is subject to change based on the
-       yet-to-be-acquired line sensor library. */
-      private:
-        int pin;
-        int threshold;
-        QTRSensorsRC bar;
-        
-      public:
-        LineSensors(int constructed_threshold) {
-          // some constructor
-          threshold = constructed_threshold;
-          //set_calibration(cMinOn,cMaxOn,cMinOff,cMaxOff);
-        }
-        
-        void LS_Setup(){
-           pinMode(pin, OUTPUT);
-        }
-        
-        byte poll_sensors() {
-          byte sensor_state = 0x00;
-          unsigned int sensor_values[8];
-          bar.read(sensor_values); //use readCalibrated if using calibration
-          
-          for (int ith = 0; ith < 8; ith++) {
-            if (sensor_values[ith] > threshold) {
-              sensor_state |= 0x01;
-            }
-            sensor_state << 1;
-          }
-          return(sensor_state);
-        }
-        
-        void set_calibration(unsigned int* set_cMinOn, unsigned int* set_cMaxOn, unsigned int* set_cMinOff, unsigned int* set_cMaxOff){
-         bar.calibratedMinimumOn = set_cMinOn;
-         bar.calibratedMaximumOn = set_cMaxOn;
-         bar.calibratedMinimumOff = set_cMinOff;
-         bar.calibratedMaximumOff = set_cMaxOff;
-        }
-         
-    };
+/****
+//LEDThresholds
+int[] cMinOn; //white with LED on
+int[] cMaxOn; //black with LED on
+int[] cMinOff; //white with LED off
+int[] cMaxOff;  //black with LED off
+****/
 
+class LineSensors {
+	/* this will be altered in the future.
+	nothing in this class is set in stone
+	and is subject to change based on the
+	yet-to-be-acquired line sensor library. */
+	private:
+		unsigned char pins[8];
+		unsigned int sensor_values[8];
+		int pin;
+		int threshold;
+		QTRSensorsRC bar;
+
+	public:
+		LineSensors(int constructed_threshold) {
+			// some constructor
+			threshold = constructed_threshold;
+			//set_calibration(cMinOn,cMaxOn,cMinOff,cMaxOff);
+			pins[0] = 14;	sensor_values[0] = 0;
+			pins[1] = 15;	sensor_values[1] = 0;
+			pins[2] = 16;	sensor_values[2] = 0;
+			pins[3] = 17;	sensor_values[3] = 0;
+			pins[4] = 18;	sensor_values[4] = 0;
+			pins[5] = 19;	sensor_values[5] = 0;
+			pins[6] = 20;	sensor_values[6] = 0;
+			pins[7] = 21;	sensor_values[7] = 0;
+			bar.init(pins, 8);
+			//	bar.calibrate();
+		}
+
+		void LS_Setup(){
+			pinMode(pin, OUTPUT);
+		}
+
+		byte poll_sensors() {
+			byte sensor_state = 0x00;
+			bar.read(sensor_values); //use readCalibrated if using calibration
+
+			for (int ith = 0; ith < 8; ith++) {
+				if (sensor_values[ith] > threshold) {
+					sensor_state |= 0x01;
+				}
+				sensor_state << 1;
+			}
+			return(sensor_state);
+		}
+
+		int value(const int index) {
+			return (sensor_values[index]);
+		}
+		
+		int read_line() {
+			int line = bar.readLine(sensor_values);
+			// 3500 is the mean return of the 8 sensor bar
+			line = 3500 - line;
+			abs(line);
+			return (line);
+		}
+
+		void set_calibration(unsigned int* set_cMinOn,
+		                     unsigned int* set_cMaxOn,
+		                     unsigned int* set_cMinOff,
+		                     unsigned int* set_cMaxOff){
+			bar.calibratedMinimumOn = set_cMinOn;
+			bar.calibratedMaximumOn = set_cMaxOn;
+			bar.calibratedMinimumOff = set_cMinOff;
+			bar.calibratedMaximumOff = set_cMaxOff;
+		}
+
+};
--- sketchbook/robot2/robot2.ino
+++ sketchbook/robot2/robot2.ino
@@ -1,3 +1,5 @@
+#include <motorCommander.h>
+
 #include <worldSensor.h>
 #include <mazeSolver.h>
 
@@ -6,7 +8,12 @@
 #include <arduinoConfig.h>
 
 byte throttle, c_round, c_part;
+char synack;
+dir::Cardinal nextMove;
 Configurator configuration;
+motorCommander mc;
+mazeSolver ms;
+
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(115200);
@@ -16,9 +23,9 @@ void setup() {
     configuration.setThrottle();
     // this blocks, waiting for the UDOO to boot
     configuration.sendConfig(&Serial);
+    
+    mc.mcSetup();
 }
-char synack;
-dir::Cardinal nextMove;
 
 void loop() {
     if (configuration.getPart() == 1) {
@@ -27,13 +34,13 @@ void loop() {
         // wait until the UDOO says move.
         } while (synack != Configurator::OP_MOVE);
         // call worldsensor;
-        nextMove = computeNextMove(listofCardinals);
+        nextMove = ms.computeNextmove(mc.get_direction());
+        Serial.write(nextMove);
     } else {
         while (Serial.available() == 0);
         // read the next cardinal from the UDOO
-        nextMove = Serial.read();
+        nextMove = dir::returnCardinal(Serial.read());
     }
-    
-    moveCardinal(nextMove);
+    mc.moveCardinal(nextMove);
     Serial.write(Configurator::OP_OK);
 }
